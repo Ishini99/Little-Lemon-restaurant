8247a7fc4ccfdceedc32b4d639a251bb3cd7a95f
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-12-26 14:18:43 +0530

    Exercise: Setting up the HTML document - completed

---
+++ web-app/src/component/Main.js
@@ -0,0 +1,13 @@
+// Main.js
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to Little Lemon</h1>
+      <p>Your favorite place for delicious food!</p>
+    </main>
+  );
+}
+
+export default Main;
